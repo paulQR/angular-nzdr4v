4ca42e84b2ff6b582d758d76618957401918bed2
paulQR <paulqr10@gmail.com>
2019-07-15 12:32:16 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-nzdr4v
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-nzdr4v)
---
+++ src/app/app.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { HelloComponent } from './hello.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+
+@NgModule({
+  imports:      [ BrowserModule, FormsModule ],
+  declarations: [ AppComponent, HelloComponent, NavbarComponent ],
+  bootstrap:    [ AppComponent ]
+})
+export class AppModule { }
