16bbb664549e4c5f0b4f97a473b31e201e23a4f8
paulQR <paulqr10@gmail.com>
2019-07-15 17:00:11 +0200

    7 files modified, 5 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,25 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
+//rutas
+import { APP_ROUTING } from './app.routes';
+
+// servicios
+
+
+
+//componentes
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
 
+import { HeroesComponent } from './components/heroes/heroes.component';
+import { AboutComponent } from './components/about/about.component';
+
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, NavbarComponent, HomeComponent ],
+  imports:      [ BrowserModule, FormsModule,APP_ROUTING ],
+  declarations: [ AppComponent, HelloComponent, NavbarComponent, HomeComponent, HeroesComponent, AboutComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
