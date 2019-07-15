f81a547474ef3f5aa455663787899fbd72486080
paulQR <paulqr10@gmail.com>
2019-07-15 15:52:09 +0200

    3 files modified, 2 added, 1 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, NavbarComponent ],
+  declarations: [ AppComponent, HelloComponent, NavbarComponent, HomeComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
