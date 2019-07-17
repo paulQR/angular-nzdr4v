f706f068080ee1764c73ab316a5d8c27a612fb49
paulQR <paulqr10@gmail.com>
2019-07-17 10:05:36 +0200

    2 files modified, 2 added

---
+++ src/app/components/heroe-tarjeta/heroe-tarjeta.component.ts
@@ -0,0 +1,13 @@
+import{Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-heroe-tarjeta',
+  templateUrl: './heroe-tarjeta.component.html', 
+})
+export class HeroeTarjetaComponent implements OnInit {
+    constructor(){}
+
+    ngOnInit(){
+
+    }
+}
