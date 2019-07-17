531d36e4070bb98179e351c01837b26e0283c2fa
paulQR <paulqr10@gmail.com>
2019-07-17 10:30:38 +0200

    1 file modified

--- src/app/components/heroe-tarjeta/heroe-tarjeta.component.ts
+++ src/app/components/heroe-tarjeta/heroe-tarjeta.component.ts
@@ -1,5 +1,5 @@
 import{Component, OnInit, Input} from '@angular/core';
-
+import{Router} from '@angular/router';
 @Component({
   selector: 'app-heroe-tarjeta',
   templateUrl: './heroe-tarjeta.component.html', 
@@ -8,13 +8,14 @@ export class HeroeTarjetaComponent implements OnInit {
     // la propiedad heroe puede venir de afuera
     @Input() heroe: any ={};
     @Input() index: number; 
-    constructor(){}
+    constructor(private router:Router){}
 
     ngOnInit(){
 
     }
 
     verHeroe(){
-      console.log(this.index);
+      //console.log(this.index);
+      this.router.navigate(['/heroe', this.index]);
     }
 }
